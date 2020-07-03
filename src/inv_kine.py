c8aa12c9931a3599fdf674660d5a5487d8f402d7
vangasse <vangasse@ic.ufal.br>
2020-07-03 16:48:49 -0300

    inverse kinematics updated according to matematical deductions

--- src/inv_kine.py
+++ src/inv_kine.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 import rospy
 import numpy as np
+import sys
 from std_msgs.msg import Float64MultiArray
 from geometry_msgs.msg import Twist
 
@@ -8,19 +9,20 @@ r = 0.0175
 d = 0.063
 
 def cb(msg):
-	v_b = np.array([float(msg.angular.z), float(msg.linear.y), -float(msg.linear.x)])
+	v_b = np.array([float(msg.angular.z), float(msg.linear.x), float(msg.linear.y)])
 	u = Float64MultiArray()
 	u.data = np.zeros(3)
-	u.data[0] = (1/r) * (-d * v_b[0] + v_b[1])
-	u.data[1] = (1/r) * (-d * v_b[0] - 0.5 * v_b[1] - np.sin(np.pi/3) * v_b[2])
-	u.data[2] = (1/r) * (-d * v_b[0] - 0.5 * v_b[1] + np.sin(np.pi/3) * v_b[2])
-	u.data = -u.data
+	u.data[0] = (1/r) * (d * v_b[0] - v_b[2])
+	u.data[1] = (1/r) * (d * v_b[0] - np.sin(np.pi/3) * v_b[1] + 0.5 * v_b[2])
+	u.data[2] = (1/r) * (d * v_b[0] + np.sin(np.pi/3) * v_b[1] + 0.5 * v_b[2])
 	pub.publish(u)
 
 rospy.init_node('inv_kine')
 
-rospy.Subscriber('/robot_1/cmd_vel', Twist, cb)
+robot = int(sys.argv[1])
 
-pub = rospy.Publisher('/robot_1/wheels_velocity_controller/command', Float64MultiArray, queue_size=10)
+rospy.Subscriber('/robot_'+ robot + '/cmd_vel', Twist, cb)
+
+pub = rospy.Publisher('/robot_'+ robot + '/wheels_velocity_controller/command', Float64MultiArray, queue_size=10)
 
 rospy.spin()
