ebc6d6c6d5417982dd5779c94872f87b5b55ead2
vangasse <vangasse@ic.ufal.br>
2020-08-05 13:46:32 -0300

    late commit must check

--- src/MPC.py
+++ src/MPC.py
@@ -68,9 +68,11 @@ class MPC:
         x_r = self.x_0
 
         # MPC objective function
-        Q_0 = sparse.diags([3.0, 3.0, 1.0, 0.0, 0.0, 0.0])
-        Q = sparse.diags([3.0, 3.0, 1.0, 0.0, 0.0, 0.0])
-        R = 0.55 * sparse.eye(self.nu)
+        calibration_param = 1
+        calibration_ori = 1
+        Q_0 = sparse.diags([calibration_param, calibration_param, calibration_ori, 0.0, 0.0, 0.0])
+        Q = sparse.diags([calibration_param, calibration_param, calibration_ori, 0.0, 0.0, 0.0])
+        R = 0.0000001 * sparse.eye(self.nu)
 
         # Casting QP format
         # QP objective
