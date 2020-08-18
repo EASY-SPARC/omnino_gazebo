0d082382bf52e4d65aef00b65e7282ff11d0798f
Vangasse <vangasse@ic.ufal.br>
2020-08-18 19:40:11 -0300

    path is calculated once, adjustment needed, hitting wall

--- src/mpc_controller.py
+++ src/mpc_controller.py
@@ -3,7 +3,8 @@
 import sys
 import rospy
 import numpy as np
-from geometry_msgs.msg import Twist, Vector3
+from geometry_msgs.msg import Twist, Vector3, PoseStamped
+from move_base_msgs.msg import MoveBaseActionGoal
 from std_msgs.msg import Float64
 from nav_msgs.msg import Odometry, Path
 from MPC import MPC
@@ -11,6 +12,7 @@ from MPC import MPC
 N = 10
 N_c = 10
 Ts = 0.1
+t = 0
 X = np.array([0., 0., 0.])
 V = np.array([0., 0., 0.])
 V_min = -5
@@ -19,8 +21,9 @@ W_min = -0.3
 W_max = 0.3
 nx = 6                      #size of a state
 
-goal = np.array([float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3])])
-robot = sys.argv[4]
+goal = np.zeros(3)
+robot = sys.argv[1]
+path = Path()
 setpoint = np.zeros((N+1)*nx)
 
 DEBUG = False
@@ -30,16 +33,38 @@ def cb_odom(msg):
     X[1] = msg.pose.pose.position.y
     X[2] = np.arctan2(2 * float(msg.pose.pose.orientation.w) * float(msg.pose.pose.orientation.z), 1 - 2 * float(msg.pose.pose.orientation.z)**2)
 
-#k*(len(msg.poses)-1)/N working     50 + k*(len(msg.poses)-51)/N    k*(len(msg.poses)-1)/N
+def cb_goal(msg):
+	global V_des
+
+	goal[0] = msg.goal.target_pose.pose.position.x
+	goal[1] = msg.goal.target_pose.pose.position.y
+	#msg.goal.target_pose.pose.position.z
+	#msg.goal.target_pose.pose.orientation.x
+	#msg.goal.target_pose.pose.orientation.y
+	#msg.goal.target_pose.pose.orientation.z
+	#msg.goal.target_pose.pose.orientation.w
+
+	# Trajectory planning
+	initial = np.copy(X)
+	t0 = 5.0
+	growth = 1
+	logistic = lambda t: 1/(1 + np.exp(- growth * (t - t0)))
+	d_logistic = lambda t: growth * logistic(t) * (1 - logistic(t))
+	V_des = lambda t: goal * d_logistic(t) - initial * d_logistic(t)
+	t = 0
+
+	data = rospy.wait_for_message('/move_base/NavfnROS/plan', Path)
+	cb_path(data)
+
 def cb_path(msg):
-    global setpoint
+    global path
     sub_sampling = 5
-    if len(msg.poses) > sub_sampling*N:
-        setpoint = np.ravel([np.append(np.array([msg.poses[sub_sampling*k].pose.position.x, msg.poses[sub_sampling*k].pose.position.y, 0.], dtype=float), np.array([0., 0., 0.], dtype=float)) for k in range(0, N+1)])
-    else:
-        setpoint = np.ravel([np.append(np.array([msg.poses[-1].pose.position.x, msg.poses[-1].pose.position.y, 0.], dtype=float), np.array([0., 0., 0.], dtype=float)) for k in range(0, N+1)])
-    #print (msg.header.seq)
-    #print("AQUI")
+
+    for k in range(0,len(msg.poses),sub_sampling):
+        path.poses.append(msg.poses[k])
+    path.poses[-1] = msg.poses[-1]
+ 
+    #setpoint = np.ravel([np.append(np.array([msg.poses[sub_sampling*k].pose.position.x, msg.poses[sub_sampling*k].pose.position.y, 0.], dtype=float), V_des(t + k * Ts)) for k in range(0, N+1)])
 
 def velocity_transform(velocity, theta):
     robot_velocity = np.array([0., 0.])
@@ -48,28 +73,22 @@ def velocity_transform(velocity, theta):
     
     return robot_velocity
 
-#if DEBUG:
-# Global path planning
-initial = np.copy(X)
-t0 = 5.0
-growth = 1
-logistic = lambda t: 1/(1 + np.exp(- growth * (t - t0)))
-d_logistic = lambda t: growth * logistic(t) * (1 - logistic(t))
-P_des = lambda t: goal * logistic(t) + initial * (1 - logistic(t))
-V_des = lambda t: goal * d_logistic(t) - initial * d_logistic(t)
-t = 0
+
 
 rospy.init_node('mpc_controller')
 
-if not DEBUG:
-	# Waiting gazebo first message /move_base/TrajectoryPlannerROS/global_plan
-	data = rospy.wait_for_message('/move_base/NavfnROS/plan', Path)
-	cb_path(data)
+# Waiting gazebo first goal message
+data = rospy.wait_for_message('/move_base/goal', MoveBaseActionGoal)
+cb_goal(data)
 
 # Subscribing on model_states instead of robot/odom, to avoid unnecessary noise
 rospy.Subscriber('/robot_'+robot+'/odom', Odometry, cb_odom)
-if not DEBUG:
-	rospy.Subscriber('/move_base/NavfnROS/plan', Path, cb_path)
+
+# Subscribing to goal
+rospy.Subscriber('/move_base/goal', MoveBaseActionGoal, cb_goal)
+
+# Subscribing to full path
+#rospy.Subscriber('/move_base/NavfnROS/plan', Path, cb_path)
 
 # Velocity publishers
 pub = rospy.Publisher('/robot_' + robot + '/cmd_vel', Twist, queue_size=10)
@@ -91,11 +110,10 @@ vel = Twist()
 while not rospy.is_shutdown():
     
     # Updating setpoint trajectory
-    if DEBUG:
-    	setpoint = np.ravel([np.append(P_des(t + k * Ts), V_des(t + k * Ts)) for k in range(0, N + 1)])
-    else:
-        for k in range(0,N):
-            setpoint[k*nx:(k+1)*nx] = setpoint[(k+1)*nx:(k+2)*nx]
+    setpoint = np.ravel([np.append(np.array([path.poses[k].pose.position.x, path.poses[k].pose.position.y, 0.], dtype=float), V_des(t + k * Ts)) for k in range(0, N+1)])
+    print(path.poses.pop(0), ">>" , path.poses[-1])
+    #for k in range(0,N):
+    	#setpoint[k*nx:(k+1)*nx] = setpoint[(k+1)*nx:(k+2)*nx]
 
     # Updating initial conditions
     controller.x_0 = np.array([X[0], X[1], X[2], V[0], V[1], V[2]])
@@ -103,16 +121,14 @@ while not rospy.is_shutdown():
     # Computing optimal input values
     [velocity, _, angular] = controller.getNewVelocity(setpoint)
 
-    [setpoint_pos.x, setpoint_pos.y, setpoint_pos.z] = P_des(t)
+    # Remember, sepoint_pos.z reffers to orientation
+    [setpoint_pos.x, setpoint_pos.y, setpoint_pos.z] = setpoint[0:3]
     orientation.data = X[2]
 
     [setpoint_vel.x, setpoint_vel.y, setpoint_vel.z] = V_des(t)
 
-    #acc = accelerationTransform(acceleration, vel.linear.x, vel.angular.z, orientation)
     velocity = velocity_transform(velocity, X[2])
 
-    #vel.linear.x = vel.linear.x + acceleration[0] * Ts
-    #vel.linear.y = vel.linear.y + acceleration[1] * Ts
     vel.linear.x = velocity[0]
     vel.linear.y = velocity[1]
     vel.angular.z = angular
